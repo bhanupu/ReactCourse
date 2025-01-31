a9505eb36868bfe4af1b852dab97f45d9b0141ef
bhanupu <bpupmanyu@gmail.com>
2025-01-31 19:19:02 +0530

    Update Made

--- 01vite/src/App.jsx
+++ 01vite/src/App.jsx
@@ -1,9 +1,13 @@
-import { useState } from 'react'
+import React from 'react'
+import Mess from './message'
 
 function App() {
 
   return (
+    <div>
     <h1> Vite Learning</h1>
+    <Mess/>
+    </div>
   )  
 }
 
---
+++ 01vite/src/message.jsx
@@ -0,0 +1,7 @@
+function Mess(){
+    return (
+        <h1>Hello Brother</h1>
+    )
+}
+
+export default Mess
