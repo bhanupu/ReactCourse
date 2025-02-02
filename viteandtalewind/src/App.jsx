853ba4ab625d2870ab9fe27da453bcc0ff977204
bhanupu <bpupmanyu@gmail.com>
2025-02-02 13:23:59 +0530

    Update Made

---
+++ viteandtalewind/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './style.css'
+import Card from './product/card'
+import Nav from './Navbars/Nav1'
+function App() {
+
+  return (
+
+    <>
+      <div>
+        <Nav />
+
+        
+
+        <Card productname="T-Shirt"  color="Blue"/>
+        <Card productname="T-Shirt"  color="Black"/>
+        <Card productname="T-Shirt"  color="Red"/>
+        <Card productname="T-Shirt"  color="Orange"/>
+        <Card productname="T-Shirt"  color="Green"/>
+        <Card productname="T-Shirt"  color="Teal"/>
+        <Card productname="T-Shirt"  color="Pink"/>
+        <Card productname="T-Shirt"  color="Mustard"/>
+        <Card productname="T-Shirt"  color="Navy"/>
+        <Card productname="T-Shirt" />
+
+      </div>
+    </>
+
+  )
+}
+
+export default App
---
+++ viteandtalewind/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+const data = ReactDOM.createRoot(document.getElementById('root'))
+data.render(
+    <App />
+)
